d30a298452daceed16de40ef24e7f8ac1870f111
JaviCM4 <javic4m@gmail.com>
2026-05-05 01:44:18 -0600

    feat: Integration for the controlled updating and deletion of posters

--- src/main/java/com/example/movies/dtos/movie/response/PosterResponse.java
+++ src/main/java/com/example/movies/dtos/movie/response/PosterResponse.java
@@ -3,13 +3,16 @@ package com.example.movies.dtos.movie.response;
 import com.example.movies.models.movie.Poster;
 import lombok.Value;
 
+import java.util.UUID;
+
 @Value
 public class PosterResponse {
 
+    UUID id;
     String urlImage;
     boolean isMain;
 
     public static PosterResponse from(Poster poster) {
-        return new PosterResponse(poster.getUrlImage(), poster.isMain());
+        return new PosterResponse(poster.getId(), poster.getUrlImage(), poster.isMain());
     }
 }
